5fa9206f0342d70ceb91866dd1eaf9ac9cd26084
Anna <areyes@milaifontanals.org>
2022-05-25 21:42:28 +0200

    25/05/2022

--- src/org/milaifontanals/interficie/IGestioProjectes.java
+++ src/org/milaifontanals/interficie/IGestioProjectes.java
@@ -7,6 +7,7 @@ package org.milaifontanals.interficie;
 
 import java.util.List;
 import org.milaifontanals.model.Projecte;
+import org.milaifontanals.model.Rol;
 import org.milaifontanals.model.Usuari;
 
 /**
@@ -18,5 +19,20 @@ public interface IGestioProjectes {
     public void closeCapa() throws GestioProjectesException;
     public void commit() throws GestioProjectesException;
     public void rollback() throws GestioProjectesException;
+    public List<Usuari> getLlistaUsuaris() throws GestioProjectesException;
+    public Usuari getUsuari(int id) throws GestioProjectesException;
+    public void addUsuari(Usuari nouUsuari) throws GestioProjectesException;
+    public void deleteUsuari(int id) throws GestioProjectesException;
+    public void modificarUsuari(Usuari usuari) throws GestioProjectesException;
+    public List<Projecte> getLlistaProjectes() throws GestioProjectesException;
+    public Projecte getProjecte(int id) throws GestioProjectesException;
+    public List<Projecte> getLlistaProjectesAssignats(Usuari usuari) throws GestioProjectesException;
+    public List<Projecte> getLlistaProjectesNoAssignats(Usuari usuari) throws GestioProjectesException;
+    public void assignarProjecte(Usuari usuari, Projecte projecte) throws GestioProjectesException;
+    public void desassignarProjecte(Usuari usuari, Projecte projecte) throws GestioProjectesException;
+    public Rol getRol(int id) throws GestioProjectesException;
+    public boolean existeixUsuari(int id) throws GestioProjectesException;
+    public boolean existeixProjecte(int id) throws GestioProjectesException;
+    
     
 }
